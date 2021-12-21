a0bffa775df20b96e898a8d73f8ae0ec8f7e787f
heewonim131 <heewonim131@gmail.com>
2021-12-21 21:15:21 +0900

    upcasting
    vc는 VIPCustomer 생성자로 인스턴스를 생성했지만
    상위 클래스인 Customer로 변수 선언하여
    Customer 타입으로 묵시적 형변환이 일어남 (업캐스팅)

--- Chapter3/src/ch03/CustomerTest.java
+++ Chapter3/src/ch03/CustomerTest.java
@@ -11,5 +11,7 @@ public class CustomerTest {
 		VIPCustomer customerKim = new VIPCustomer(10020, "김유신");
 		customerKim.bonusPoint = 10000;
 		System.out.println(customerKim.showCustomerInfo());
+		
+		Customer vc = new VIPCustomer(12345, "noname");
 	}
 }
