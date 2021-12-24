6ed156a0b9bf2fd6e70cb552b47b22032d7fb2cf
heewonim131 <heewonim131@gmail.com>
2021-12-24 17:44:49 +0900

    Fix variable declaration to constructor DI
    변수 선언과 동시에 객체를 생성하는 코드를
    디폴트 생성자를 통해 의존성 주입하는 코드로 변경

--- Chapter3/src/ch16/gamelevel/Player.java
+++ Chapter3/src/ch16/gamelevel/Player.java
@@ -2,7 +2,13 @@ package ch16.gamelevel;
 
 public class Player {
 
-	PlayerLevel level = new BeginnerLevel();
+//	PlayerLevel level = new BeginnerLevel();
+	private PlayerLevel level;
+	
+	public Player() {	// 생성자를 통한 의존성 주입
+		level = new BeginnerLevel();
+		level.showLevelMessage();
+	}
 	
 	public PlayerLevel getLevel() {
 		return level;
