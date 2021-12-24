fe72af1858c310b44b993c25ce5eb565a6f8518f
heewonim131 <heewonim131@gmail.com>
2021-12-24 19:43:11 +0900

    String class

---
+++ Chapter4/src/ch03/StringTextBlock.java
@@ -0,0 +1,34 @@
+package ch03;
+
+public class StringTextBlock {
+
+	public static void main(String[] args) {
+		
+		// """text block""" (Java 13~)
+		/*
+		String strBlock = """
+				This 
+				is 
+				text
+				block
+				test.""";
+		System.out.println(strBlock);
+		
+		System.out.println(getBlockOfHtml());
+		*/
+	}
+	
+	// html, json 문자열을 만드는 데 유용
+	/*
+	public static String getBlockOfHtml() {
+		    return """
+		            <html>
+
+		                <body>
+		                    <span>example text</span>
+		                </body>
+		            </html>""";
+		
+	}
+	*/
+}
