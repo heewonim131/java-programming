f10c924f628cfcc49354760eb4bae3ed9fef981b
heewonim131 <heewonim131@gmail.com>
2021-12-24 17:42:37 +0900

    Add final keyword
    템플릿 메서드는 final로 선언하여
    하위 클래스에서 재정의 할 수 없도록  한다.

--- Chapter3/src/ch16/gamelevel/PlayerLevel.java
+++ Chapter3/src/ch16/gamelevel/PlayerLevel.java
@@ -7,7 +7,8 @@ public abstract class PlayerLevel {
 	public abstract void turn();
 	public abstract void showLevelMessage();
 	
-	public void go(int count) {
+//	템플릿 메서드 final로 선언하여 재정의 할 수 없도록
+	final public void go(int count) {
 		run();
 		for (int i = 0; i < count; i++) {
 			jump();			
