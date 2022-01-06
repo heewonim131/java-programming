42199e580019a1945f6120bb0622435d43825f24
heewonim131 <heewonim131@gmail.com>
2022-01-06 22:46:03 +0900

    Create grade report class

---
+++ Chapter7/src/school/School.java
@@ -0,0 +1,42 @@
+package school;
+
+import java.util.ArrayList;
+
+public class School {
+
+	// fields
+	private static School instance = new School();
+	private static String SCHOOL_NAME = "Good School";
+	private ArrayList<Student> studentList = new ArrayList<Student>();
+	private ArrayList<Subject> subjectList = new ArrayList<Subject>();
+	
+	private School(){}
+	
+	public static School getInstance(){
+		if(instance == null) 
+			instance = new School();
+		return instance;
+	}
+	
+	// add method
+	public void addStudent(Student student){
+		studentList.add(student);
+	}
+
+	public void addSubject(Subject subject) {
+		subjectList.add(subject);
+	}
+
+	// getter, setter
+	public ArrayList<Student> getStudentList(){
+		return studentList;
+	}
+
+	public ArrayList<Subject> getSubjectList() {
+		return subjectList;
+	}
+
+	public void setSubjectList(ArrayList<Subject> subjectList) {
+		this.subjectList = subjectList;
+	}
+}
