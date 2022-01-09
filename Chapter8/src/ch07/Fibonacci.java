335b4a3dcfb2c4d29c44c6aed185e44a12eb5854
heewonim131 <heewonim131@gmail.com>
2022-01-09 15:27:16 +0900

    Solve Fibonacci using memoization

--- Chapter8/src/ch07/Fibonacci.java
+++ Chapter8/src/ch07/Fibonacci.java
@@ -3,8 +3,11 @@ package ch07;
 public class Fibonacci {
 
 	int number;
+	int[] value;
+	
 	public Fibonacci(int number) {
 		this.number = number;
+		value = new int[number];
 	}
 
 	// 1) 재귀 함수
@@ -31,6 +34,21 @@ public class Fibonacci {
 		return cur;
 	}
 	
+	// 3) 메모이제이션
+	public int fiboMem(int n) {
+		value[0] = 0;
+		value[1] = 1;
+		
+		if (n == 0) return value[0];
+		if (n == 1) return value[1];
+		
+		int i = 0;
+		for (i = 2; i <= n; i++) {
+			value[i] = value[i-1] + value[i-2];
+		}
+		return value[i-1];
+	}
+	
 	public static void main(String[] args) {
 		
 		Fibonacci fib = new Fibonacci(100);
@@ -39,6 +57,9 @@ public class Fibonacci {
 		System.out.println(result);
 		
 		result = fib.fiboIter(10);
+		System.out.println(result);
+		
+		result = fib.fiboMem(10);
 		System.out.println(result);
 	}
 	
