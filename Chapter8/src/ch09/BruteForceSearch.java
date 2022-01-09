72ccdd6ceddbbd9a355714b32934b1ab9afa1790
heewonim131 <heewonim131@gmail.com>
2022-01-09 16:01:07 +0900

    Find count of numbers

--- Chapter8/src/ch09/BruteForceSearch.java
+++ Chapter8/src/ch09/BruteForceSearch.java
@@ -15,9 +15,9 @@ public class BruteForceSearch {
 			for (i1 = i0; i1 >= 0; i1 -= bills[1]) {
 				for (i2 = i1; i2 >= 0; i2 -= bills[2]) {
 					for (i3 = i2; i3 >= 0; i3 -= bills[3]) {
-						for (i4 = i3; i4 >= 0; i4 -= bills[4])
-							if (i4 % bills[5] == 0)
-								count++;
+						for (i4 = i3; i4 >= 0; i4 -= bills[4]) {
+							if (i4 % bills[5] == 0) count++;
+						}
 					}
 				}
 			}
