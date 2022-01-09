eed90c8c3c54b4386629cca521e4a857360513cf
heewonim131 <heewonim131@gmail.com>
2022-01-09 15:21:20 +0900

    Solve Fibonacci using iteration

--- Chapter8/src/ch07/Fibonacci.java
+++ Chapter8/src/ch07/Fibonacci.java
@@ -13,12 +13,32 @@ public class Fibonacci {
 		if (n == 1) return 1;
 		return fiboRecur(n-1) + fiboRecur(n-2);
 	}
+
+	// 2) 반복문
+	public int fiboIter(int n) {
+		int ppre = 0;
+		int pre = 1;
+		int cur = 0;
+		
+		if (n == 0) return 0;
+		if (n == 1) return 1;
+		
+		for (int i = 2; i <= n; i++) {
+			cur = ppre + pre;
+			ppre = pre;
+			pre = cur;
+		}
+		return cur;
+	}
 	
 	public static void main(String[] args) {
 		
 		Fibonacci fib = new Fibonacci(100);
 		
 		int result = fib.fiboRecur(10);
+		System.out.println(result);
+		
+		result = fib.fiboIter(10);
 		System.out.println(result);
 	}
 	
